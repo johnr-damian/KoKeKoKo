050d2646792b2909653128c3d4f0f7b9613d1a6f
John Rhey Damian <johnr.damian98@gmail.com>
2019-07-25 17:36:31 +0800

    thinking about the flow of the program when to call model, evaluate envi, and take action

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #define NOMINMAX
 
 #include <fstream>
-#include <iostream>	
+#include <iostream>
 #include <sc2api/sc2_api.h>
 #include <sstream>
 #include <thread>
@@ -10,95 +10,100 @@
 namespace KoKeKoKo
 {
 	using namespace std;
-	const string MODELSERVICE_FILENAME = "ModelService\\bin\\Release\\ModelService.exe";				//Contains the POMDP Model, MCTS Model, and both
-	const string REPOSITORY_FILENAME = "Documents\\Data\\Repository.csv";					//The parsed and combination of CommandsRepository and ResourcesRepository
-	const string COMMANDSREPOSITORY_FILENAME = "Documents\\Data\\CommandsRepository.csv";	//The preprocessed training data from replay files
-	const string RESOURCESREPOSITORY_FILENAME = "Documents\\Data\\ResourcesRepository.csv";	//The preprocessed training data from replay files
+	//The POMDP Model Computation and MCTS Model Computation using R
+	const string MODELSERVICE_FILENAME = "ModelService\\bin\\Release\\ModelService.exe";
+	//The parsed and combination of COMMANDSREPOSITORY and RESOURCESREPOSITORY
+	const string REPOSITORYSERVICE_FILENAME = "Documents\\Data\\Repository.csv";
+	//The extracted commands from the replay data
+	const string COMMANDSREPOSITORY_FILENAME = "Documents\\Data\\CommandsRepository.csv";
+	//The extracted resources from the replay data
+	const string RESOURCESREPOSITORY_FILENAME = "Documents\\Data\\ResourcesRepository.csv";
 	class ModelRepositoryService
 	{
 		private:
-			static ModelRepositoryService* _instance;						//The instance of the service
+			//The instance of ModelRepositoryService
+			static ModelRepositoryService* _instance;
 
-			//Constructor
 			ModelRepositoryService()
+			//Gets the current project directory. Afterwards, parses the COMMANDSREPOSITORY and RESOURCESREPOSITORY
 			{
 				try
 				{
-					IsSuccessfulInstance = (GetCurrentDirectory(MAX_PATH, CurrentProjectDirectory) != 0);
+					if (GetCurrentDirectory(MAX_PATH, CurrentProjectDirectory) != 0)
+					{
+						IsSuccessfulInstance = true;
+					}
+					else
+						throw new exception("Failed to get current directory...");
 				}
 				catch (...)
 				{
-					cout << "Error Occurred! Failed to get current project directory..." << endl;
+					cout << "Error Occurred! Failed to create instance of ModelRepositoryService..." << endl;
 					cerr << "Error Occurred! ModelRepositoryService -> ModelRepositoryService()" << endl;
 					IsSuccessfulInstance = false;
 				}
 			}
 
-			//Reads the COMMANDSREPOSITORY_FILENAME and Creates a tally of commands by rank
-			void ParseCommandsRepository(bool* is_successful)
+			void ParseCommandsRepository()
 			{
+
+			}
+
+			void ParseResourcesRepository()
+			{
+
+			}
+
+			int RunExecutableFile(string executable_filename)
+			//Creates a process and runs the executable file
+			{
+				STARTUPINFO startupinfo;
+				PROCESS_INFORMATION processinformation;
+				DWORD dword;
+				LPSTR executablefilepath = new char[MAX_PATH];
+				int processresult = -1;
+
 				try
 				{
-					ifstream commandsrepository(GetRelativeFilepathOf(COMMANDSREPOSITORY_FILENAME));
-					string commandsrepositoryline = "", commandsrepositorycomma = "", previouscommand = "";
+					ZeroMemory(&startupinfo, sizeof(startupinfo));
+					startupinfo.cb = sizeof(startupinfo);
+					ZeroMemory(&processinformation, sizeof(processinformation));
+					executablefilepath = const_cast<char *>(executable_filename.c_str());
 
-					if (commandsrepository.is_open())
+					if (CreateProcessA(NULL, executablefilepath, NULL, NULL, FALSE, 0, NULL, NULL, &startupinfo, &processinformation))
 					{
-						cout << "Successful! Opened " << GetRelativeFilepathOf(COMMANDSREPOSITORY_FILENAME) << "..." << endl;
-						while (getline(commandsrepository, commandsrepositoryline))
-						//Read line by line
-						{
-							stringstream streambyline(commandsrepositoryline);
-							for (int column = 0; getline(streambyline, commandsrepositorycomma, ','); column++)
-							{
-								if (column == 2)
-								//The current column is a command
-								{
-									//Initialize previous command
-									if (previouscommand == "")
-										previouscommand = commandsrepositorycomma;
-
-									//Check if that command is existing
-									if (_toberevisedcommands.find(commandsrepositorycomma) == _toberevisedcommands.end())
-									{
-										_toberevisedcommands.insert(make_pair(commandsrepositorycomma, vector<string>()));
-										_toberevisedlist.insert(make_pair(commandsrepositorycomma, 0));
-									}
-
-									//Update fields
-									previouscommand = commandsrepositorycomma;
-								}
-							}
-						}
-						cout << "Successful! Parsed " << GetRelativeFilepathOf(COMMANDSREPOSITORY_FILENAME) << "..." << endl;
-						commandsrepository.close();
-						*is_successful = true;
-					}					
+						WaitForSingleObject(processinformation.hProcess, INFINITE);
+						processresult = GetExitCodeProcess(processinformation.hProcess, &dword);
+						CloseHandle(processinformation.hProcess);
+						CloseHandle(processinformation.hThread);
+					}
+					else
+						throw new exception("Failed to create process...");
 				}
 				catch (...)
 				{
-					cout << "Error Occurred! Failed to parse commands repository..." << endl;
-					cerr << "Error Occurred! ModelRepositoryService -> ParseCommandsRepository()" << endl;
-					*is_successful = false;
+					cout << "Error Occurred! Failed to run the executable file..." << endl;
+					cerr << "Error Occurred! ModelRepositoryService -> RunExecutableFile(" << executable_filename << ")" << endl;
+					processresult = -1;
 				}
+
+				return processresult;
 			}
 
-			//Reads the RESOURCESREPOSITORY_FILENAME and Creates a sequence of resources by rank
-			void ParseResourcesRepository(bool* is_successful)
+			string GetRelativeFilepathOf(string filename)
+			//Returns the filename concatenated with the current project directory
 			{
-
+				return ((string)CurrentProjectDirectory) + "\\" + filename;
 			}
 
 		public:
-			bool IsSuccessfulInstance = false;								//If the creation of ModelRepositoryService instance is successful
-			LPSTR CurrentProjectDirectory = new char[MAX_PATH];				//Contains the project directory
+			//If the instance of ModelRepositoryService is successful
+			bool IsSuccessfulInstance = false;
+			//A string that pertains to the current project directory
+			LPSTR CurrentProjectDirectory = new char[MAX_PATH];
 
-			map<string, vector<string>> _toberevisedcommands = map<string, vector<string>>();
-			map<string, int> _toberevisedlist = map<string, int>();
-			map<string, int> _toberevisedrepo = map<string, int>();
-
-			//Returns the created ModelRepositoryService instance
 			static ModelRepositoryService* GetModelRepositoryServiceInstance()
+			//Returns the created instance of ModelRepositoryService
 			{
 				if (_instance == nullptr)
 					_instance = new ModelRepositoryService();
@@ -108,117 +113,61 @@ namespace KoKeKoKo
 
 			bool GenerateRepository()
 			{
-				bool parsecommands_successful = false, parseresources_successful = false;				
-				auto test = new thread(&ModelRepositoryService::ParseCommandsRepository, this, &parsecommands_successful);
-				ofstream repo(GetRelativeFilepathOf(REPOSITORY_FILENAME));
-				int fileline = 0;
-				if (repo.is_open())
-				{
-					repo << "Example of file line" << endl;
-					++fileline;
-					repo << "Example of file line" << endl;
-					++fileline;
-					repo << "Example of file line" << endl;
-					++fileline;
-					repo << "Example of file line" << endl;
-					++fileline;
-					repo << "Example of file line" << endl;
-					++fileline;
-					repo << "Example of file line" << endl;
-					++fileline;
-					repo << "Example of file line" << endl;
-					++fileline;
-					repo << "Example of file line" << endl;
-					++fileline;
-					
-					if (test->joinable())
-					{
-						test->join();
-						if (!parsecommands_successful)
-							return false;
-						repo << endl;
-						++fileline;
-						repo << endl;
-						++fileline;
 
-						repo << "Bronze" << endl;
-						_toberevisedrepo.insert(make_pair("Bronze", fileline));
-						++fileline;
-						for (auto states : _toberevisedlist)
-						{
-							repo << states.first << ",";
-						}
-						_toberevisedrepo.insert(make_pair("BronzeStates", fileline));
-						++fileline;
-						for (auto yaxis : _toberevisedcommands)
-						{
-							for (auto xaxis : _toberevisedlist)
-								repo << count(yaxis.second.begin(), yaxis.second.end(), xaxis.first) << ",";
-						}
-						_toberevisedrepo.insert(make_pair("BronzeTransition", fileline));
-						++fileline;
-						repo.close();
-						return true;
-					}
-				}
+			}
+
+			int ExecuteModelService()
+			//Calls the ModelService.exe and returns the AbilityType ID
+			{
+				int abilityid = RunExecutableFile(KoKeKoKo::MODELSERVICE_FILENAME);
 				
-				return false;
-			}
-
-			//Returns the cocanated current directory and filename
-			string GetRelativeFilepathOf(string filename)
-			{
-				return ((string)CurrentProjectDirectory) + "\\" + filename;
-			}
-
-			//Creates REPOSITORY_FILENAME. Stores the important lines in the file. Generates all important data for the model
-			//Getcorrespondingenum
-			double RunExecutableFile(std::string executable_file)
-			{
-				cout << executable_file << endl;
-				STARTUPINFO startupinfo;
-				PROCESS_INFORMATION processinformation;
-				DWORD dword;
-				LPSTR executablefilepath = new char[MAX_PATH];
-				double processresult = -1;
-
-				try
-				{
-					ZeroMemory(&startupinfo, sizeof(startupinfo));
-					startupinfo.cb = sizeof(startupinfo);
-					ZeroMemory(&processinformation, sizeof(processinformation));
-					executablefilepath = const_cast<char *>(executable_file.c_str());
-
-					if (CreateProcessA(NULL, executablefilepath, NULL, NULL, FALSE, 0, NULL, NULL, &startupinfo, &processinformation))
-					{
-						std::cout << "Successful Execution! Running " << executablefilepath << std::endl;
-
-						WaitForSingleObject(processinformation.hProcess, INFINITE);
-						processresult = GetExitCodeProcess(processinformation.hProcess, &dword);
-						CloseHandle(processinformation.hProcess);
-						CloseHandle(processinformation.hThread);
-					}
-					else
-						throw processresult;
-				}
-				catch (...)
-				{
-					std::cout << "Error Occurred! Failed to execute the executable file..." << std::endl;
-					std::cerr << "Error Occurred! ProgramUtilities -> RunExecutableFile" << std::endl;
-				}
-
-				return processresult;
 			}
 	};
+
 
 	using namespace sc2;
 	class KoKeKoKoBot : public Agent
 	{
 		private:
-			//TryToExecuteAbility
-			//TryToFind
-			//More Utility function
+
 		public:
+			virtual void OnGameStart() final
+			//On game start, we need to create the MCTS tree, start execution of POMDP and create the combination of the two
+			{
+				auto modelrepositoryservice = KoKeKoKo::ModelRepositoryService::GetModelRepositoryServiceInstance();
+				//Create the MCTS and start the expansion to get the sequence of actions
+				//Start the POMDP to get the sequence of actions
+				//Create the MCTS+POMDP and start the expansion
+
+				//We store the actions in a queue
+			}
+
+			virtual void OnStep() final
+			{
+				//Observe the environment
+				//Is it already 20 secs? 
+					//If yes, we continue again the MCTS, start POMDP, and create the MCTS+POMDP
+					//If no, keep following the actions in queue
+					//If no but there is an emergency, we call the MCTS, POMDP and the combination to know what to do
+
+				/* For Guanga */
+				//Make the bot, train scv, build supply depot then build barracks then train marine
+				//After sufficient marine, attack the enemy. Follow this in summary -> https://github.com/Blizzard/s2client-api/blob/master/docs/tutorial1.md
+			}
+
+			virtual void OnUnitIdle(const Unit* unit) final
+			{
+
+			}
+
+			/* For Guanga */
+			//Create these functions in public
+				//TryToExecuteAction - will receive an input (can be string or ABILITYTYPE ID) and execute the corresponding private function
+				//FindNearestOf - will receive two input, the source and target. find the nearest target from the source
+				//CountOf - will receive an input (UNITYPEID). returns the count of that unit
+			//Create these functions in private
+				//Execute{Ability} - the corresponding action in TryToExecuteAction
+					//example: TryBuildBarracks, TryBuildSupplyDepot
 
 	};
 }
@@ -226,25 +175,25 @@ namespace KoKeKoKo
 KoKeKoKo::ModelRepositoryService* KoKeKoKo::ModelRepositoryService::_instance = nullptr;
 int main(int argc, char* argv[])
 {
-	KoKeKoKo::KoKeKoKoBot* kokekokobot = new KoKeKoKo::KoKeKoKoBot();
-	sc2::Coordinator* coordinator = new sc2::Coordinator();
-	KoKeKoKo::ModelRepositoryService* modelrepositoryservice = KoKeKoKo::ModelRepositoryService::GetModelRepositoryServiceInstance();
+	auto kokekokobot = new KoKeKoKo::KoKeKoKoBot();
+	auto coordinator = new sc2::Coordinator();
+	auto modelrepositoryservice = KoKeKoKo::ModelRepositoryService::GetModelRepositoryServiceInstance();
 	char s;
 
-	std::cout << modelrepositoryservice->CurrentProjectDirectory << std::endl;
-	if (modelrepositoryservice->GenerateRepository())
+	if (!(modelrepositoryservice->IsSuccessfulInstance))
 	{
-		int stateline = modelrepositoryservice->_toberevisedrepo["BronzeStates"];
-		int transitionline = modelrepositoryservice->_toberevisedrepo["BronzeTransition"];
-		int result = modelrepositoryservice->RunExecutableFile(modelrepositoryservice->GetRelativeFilepathOf(KoKeKoKo::MODELSERVICE_FILENAME + " " + std::to_string(stateline) + " " + std::to_string(transitionline)));
-
-		std::cout << result << std::endl;
+		std::cout << "There is a problem in the ModelRepositoryService, please try to resolve the issue to start the game." << std::endl;
+		std::cin >> s;
+		return -1;
 	}
-	//system("");
-	//modelrepositoryservice->RunExecutableFile("C:\\Users\\Kelsey\\source\\repos\\KoKeKoKo\\SC2API_Binary_vs2017\\RProject1\\Script.R");
-	//int test = system("C:\\Users\\Kelsey\\source\\repos\\KoKeKoKo\\SC2API_Binary_vs2017\\RProject1\\Script.R");
-	//std::cout << test << std::endl;
+
+	std::cout << "Preparing StarCraft II..." << std::endl;
 	std::cin >> s;
+	coordinator->LoadSettings(argc, argv);
+	coordinator->SetParticipants({ sc2::CreateParticipant(sc2::Race::Terran, kokekokobot), sc2::CreateComputer(sc2::Race::Terran, sc2::Difficulty::VeryEasy) });
+	coordinator->LaunchStarcraft();
+	coordinator->StartGame(sc2::kMapBelShirVestigeLE);
+	while (coordinator->Update());
 
 	return 0;
 }
